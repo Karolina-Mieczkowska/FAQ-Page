607a8115317eb6d255f0f3ad91d5547615331b5b
Karolina <kmieczkowska75@gmail.com>
2021-05-13 23:55:21 +0100

    Add Accordion toggle functionality

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const accordionItems = document.querySelectorAll('.accordion-item');
+const accordionBodyItems = document.querySelectorAll('.accordion-body');
 
+function addClass(el, klass) {
+    el.classList.add(klass);
+}
+
+function removeClass(el, klass) {
+    el.classList.remove(klass);
+}
+
+accordionItems[0].querySelector('.accordion-body').classList.add('accordion-body-active');
+
+accordionItems.forEach(function(ind) {
+    const accordionButton = ind.firstElementChild;
+    accordionButton.addEventListener('click', AccordionToggle);
+})
+
+function AccordionToggle(event) {
+    accordionBodyItems.forEach(function(accordionBody) {
+        if (accordionBody.previousElementSibling === event.target) {
+            addClass(accordionBody, 'accordion-body-active');
+        } else {
+            removeClass(accordionBody, 'accordion-body-active');
+        }
+    })
+}
