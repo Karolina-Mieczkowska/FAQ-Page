07aac1ca869794f5d0b7dccef6a437371d44c7d4
Karolina <kmieczkowska75@gmail.com>
2021-05-14 22:37:52 +0100

    Experiment width different toggle functionalities

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const accordionBodyItems = document.querySelectorAll('.accordion-body');
 const buttonItems = document.querySelectorAll('.accordion-button');
 const arrowItems = document.querySelectorAll('.arrow-down');
 
+accordionItems.forEach(function(ind) {
+    ind.firstElementChild.nextElementSibling.style.transform = 'translateY(-100%)';
+})
+
+// buttonItems (na jutro)
+
 
 accordionItems.forEach(function(ind, key) {
     ind.addEventListener('click', function() {
     // const activeAccordion = accordionItems[key];
     // activeAccordion.firstElementChild.nextElementSibling.classList.toggle('accordion-body-active');
     arrowItems[key].classList.toggle('arrow-up');
-    accordionItems[key].classList.toggle('accordion-body-active');
+    const activeAccordion = accordionItems[key];
+    // accordionItems[key].classList.toggle('accordion-body-active');
+    accordionItems[key].firstElementChild.nextElementSibling.style.transform = 'translateY(0)';
+    accordionItems[key].firstElementChild.nextElementSibling.style.opacity = '1';
     })
 }) 
 
