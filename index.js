ec0fe3ff45f14d0ba2ba330bc3e73307c63ad6d3
Karolina <kmieczkowska75@gmail.com>
2021-05-14 21:31:18 +0100

    update toggle functionality

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 const accordionItems = document.querySelectorAll('.accordion-item');
 const accordionBodyItems = document.querySelectorAll('.accordion-body');
+const buttonItems = document.querySelectorAll('.accordion-button');
+const arrowItems = document.querySelectorAll('.arrow-down');
 
-function addClass(el, klass) {
-    el.classList.add(klass);
-}
 
-function removeClass(el, klass) {
-    el.classList.remove(klass);
-}
-
-accordionItems[0].querySelector('.accordion-body').classList.add('accordion-body-active');
-
-accordionItems.forEach(function(ind) {
-    const accordionButton = ind.firstElementChild;
-    accordionButton.addEventListener('click', AccordionToggle);
-})
-
-function AccordionToggle(event) {
-    accordionBodyItems.forEach(function(accordionBody) {
-        if (accordionBody.previousElementSibling === event.target) {
-            addClass(accordionBody, 'accordion-body-active');
-        } else {
-            removeClass(accordionBody, 'accordion-body-active');
-        }
+accordionItems.forEach(function(ind, key) {
+    ind.addEventListener('click', function() {
+    // const activeAccordion = accordionItems[key];
+    // activeAccordion.firstElementChild.nextElementSibling.classList.toggle('accordion-body-active');
+    arrowItems[key].classList.toggle('arrow-up');
+    accordionItems[key].classList.toggle('accordion-body-active');
     })
-}
+}) 
+
+
+
 
 // testing branching
