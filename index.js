23bee9e3cb13e05d2503d6b3939bb20f071acbf8
Karolina <kmieczkowska75@gmail.com>
2021-05-19 12:23:04 +0100

    Add accordion animation based on bootstrap pattern

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
-const accordionItems = document.querySelectorAll('.accordion-item');
-const accordionBodyItems = document.querySelectorAll('.accordion-body');
-const buttonItems = document.querySelectorAll('.accordion-button');
-const arrowItems = document.querySelectorAll('.arrow-down');
+const accordionButton = document.querySelectorAll('.accordion-button');
+const accordionSection = document.querySelectorAll('.accordion-item');
 
+// accordionSection.forEach((item) => {
+//     item.addEventListener('mouseover', (event) => {
+//         item.firstElementChild.lastElementChild.firstElementChild.style.color = '#132C33';
+//     })
+//     item.addEventListener('mouseout', (event) => {
+//         item.firstElementChild.lastElementChild.firstElementChild.style.color = '#D8E3E7';
+//     })
+// })
 
-accordionItems.forEach(function(ind, key) {
-    ind.addEventListener('click', function() {
-    // const activeAccordion = accordionItems[key];
-    // activeAccordion.firstElementChild.nextElementSibling.classList.toggle('accordion-body-active');
-    arrowItems[key].classList.toggle('arrow-up');
-    const activeAccordion = accordionItems[key];
-    accordionItems[key].classList.toggle('accordion-body-active');
+// Dodaj if do mouseover!
+
+accordionSection.forEach((item) => {
+    item.addEventListener('mouseover', (event) => {
+        console.log(item.firstElementChild);
     })
-}) 
+})
 
+accordionButton.forEach((item) => {
+    item.addEventListener('click', () => {
+        let accordionCollapse = item.nextElementSibling;
+        
+        if (!item.classList.contains('collapsing')) {
+            if (!item.classList.contains('open')) {
+                accordionCollapse.style.display = 'block';
+                let accordionHeight = accordionCollapse.clientHeight;
+    
+                setTimeout(() => {
+                    accordionCollapse.style.height = accordionHeight + 'px';
+                    accordionCollapse.style.display = '';
+                }, 1);
 
+                accordionCollapse.classList = 'accordion-collapse collapsing';
 
+                setTimeout(() => {
+                    accordionCollapse.classList = 'accordion-collapse collapse open';
+                }, 500)
+            } else {
+                accordionCollapse.classList = 'accordion-collapse collapsing';
 
-// testing branching
-// bootstrap branch testing
+                setTimeout(() => {
+                    accordionCollapse.style.height = '0px';
+                }, 1);
+    
+                setTimeout(() => {
+                    accordionCollapse.classList = 'accordion-collapse collapse';
+                    accordionCollapse.style.height = '';
+                }, 500);
+
+            }
+
+            item.classList.toggle('open')
+            item.lastElementChild.firstElementChild.classList.toggle('arrow-up');
+        }
+    })
+})
